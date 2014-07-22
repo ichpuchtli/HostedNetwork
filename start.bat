b53c928cee51837535573ebe3a0ad155f31facc7
Sam <sam.mack91@gmail.com>
2014-07-22 14:27:12 +1000

    fixed ssid typo

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-netsh wlan set hostednetwork mode=allow ssid="AcessPointSSID" key="Password" keyUsage=persistent
+netsh wlan set hostednetwork mode=allow ssid="AccessPointSSID" key="Password" keyUsage=persistent
 netsh wlan start hostednetwork
 netsh wlan show hostednetwork
 pause
