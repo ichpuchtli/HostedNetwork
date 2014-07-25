c86bce8e87b5a3d94761a26fc8bee732955c7812
Sam <sam.mack91@gmail.com>
2014-07-25 14:06:36 +1000

    stop the hostednetwork before startng

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 @echo off
+netsh wlan stop hostednetwork
 netsh wlan set hostednetwork mode=allow ssid="AccessPointSSID" key="Password" keyUsage=persistent
 netsh wlan start hostednetwork
 netsh wlan show hostednetwork
